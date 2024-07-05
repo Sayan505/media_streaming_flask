7389747050ffbe2d2707c2ac16b3ce1406af5f9a
Sayan Bhattacharjee <bsayan8086@gmail.com>
2024-07-06 04:39:36 +0530

    fix: refactor user role enum

--- backend/models/user_model.py
+++ backend/models/user_model.py
@@ -5,11 +5,11 @@ from sqlalchemy.orm import Mapped, mapped_column
 from config.orm     import db
 
 
-class UserTypeEnum(enum.Enum):
-    Banned       = 0
-    Listener     = 1
-    Artist       = 2
-    Admin        = 3
+class UserRoleEnum(enum.Enum):
+    Banned   = 0
+    Listener = 1
+    Artist   = 2
+    Admin    = 3
 
 class User(db.Model):
     __tablename__ = "users"
@@ -17,5 +17,5 @@ class User(db.Model):
     id:          Mapped[int]          = mapped_column(primary_key=True, autoincrement=True)
     oauth_sub:   Mapped[str]          = mapped_column(nullable=False, unique=True)
     displayname: Mapped[str]          = mapped_column(nullable=False)
-    user_type:   Mapped[UserTypeEnum] = mapped_column(default=UserTypeEnum.Listener)
+    user_role:   Mapped[UserRoleEnum] = mapped_column(nullable=False, default=UserRoleEnum.Listener)
 
