a823ce606f34c058aeae057855fef17ce2cfaf29
Sayan Bhattacharjee <bsayan8086@gmail.com>
2024-07-07 19:00:38 +0530

    fix: use enum strings for user role instead of enum type

--- backend/models/user_model.py
+++ backend/models/user_model.py
@@ -6,16 +6,16 @@ from config.orm     import db
 
 
 class UserRoleEnum(enum.Enum):
-    Banned   = 0
-    Listener = 1
-    Artist   = 2
-    Admin    = 3
+    Banned   = "banned"
+    Listener = "listener"
+    Artist   = "artist"
+    Admin    = "admin"
 
 class User(db.Model):
     __tablename__ = "users"
 
-    id:          Mapped[int]          = mapped_column(primary_key=True, autoincrement=True)
-    oauth_sub:   Mapped[str]          = mapped_column(nullable=False, unique=True)
-    displayname: Mapped[str]          = mapped_column(nullable=False)
-    user_role:   Mapped[UserRoleEnum] = mapped_column(nullable=False, default=UserRoleEnum.Listener)
+    id:          Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    oauth_sub:   Mapped[str] = mapped_column(nullable=False, unique=True)
+    displayname: Mapped[str] = mapped_column(nullable=False)
+    user_role:   Mapped[str] = mapped_column(nullable=False, default=UserRoleEnum.Listener.value)
 
