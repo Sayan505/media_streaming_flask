f52b0f8f59ee1f5a8dabf48038aac88068681d23
Sayan Bhattacharjee <bsayan8086@gmail.com>
2024-07-09 15:22:39 +0530

    fix: config user info display

--- backend/modules/user_module.py
+++ backend/modules/user_module.py
@@ -1,15 +1,23 @@
-from config.logger      import log
-
-from flask_jwt_extended import jwt_required, get_jwt_identity 
-
 from flask import Blueprint
 blueprint = Blueprint("user_module", __name__)
+
+
+from config.orm        import db
+from models.user_model import User
+
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 
 @blueprint.route("/api/me", methods=["GET"])
 @jwt_required()
 def myinfo():
-    sub = get_jwt_identity()
-    
-    return sub, 200
+    oauth_sub = get_jwt_identity()
+    user      = db.session.execute(db.select(User).filter_by(oauth_sub=oauth_sub)).scalar_one_or_none()
+    if not user:
+        return { "status": "invalid oauth identity" }, 401
+
+    return {
+        "display_name": user.display_name,
+        "email": user.email
+    }, 200
 
--- frontend/app/me/page.js
+++ frontend/app/me/page.js
@@ -6,13 +6,13 @@ import axios_client from "../../axios_instance.js";
 
 export default function Me() {
     const [alleged_role, set_alleged_role] = useState(false);
-    const [response, set_response] = useState(false);
+    const [user_data, set_user_data] = useState(false);
 
     useEffect(() => {
         set_alleged_role(localStorage.getItem("role"));
         
         axios_client.get("/api/me").then((res) => {
-            set_response(res.data);
+            set_user_data(res.data);
         });
     }, []);
 
@@ -20,7 +20,8 @@ export default function Me() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <div>
-                <div className="inline size-96 break-all"> response: {response} </div>
+                <div className="inline size-96 break-all"> {user_data.display_name} </div>
+                <div className="inline size-96 break-all"> {user_data.email} </div>
                 <br/><br/><br/>
                 <div className="inline size-96 break-all"> alleged role: {alleged_role} </div>
             </div>
