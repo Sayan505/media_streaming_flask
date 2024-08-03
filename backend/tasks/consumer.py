8485b431ff25106d419d4dc92e763ef43c1c534b
Sayan Bhattacharjee <bsayan8086@gmail.com>
2024-08-04 01:20:44 +0530

    feat: implement HLS encoding worker

--- backend/tasks/consumer.py
+++ backend/tasks/consumer.py
@@ -1,9 +1,11 @@
 import os
+import subprocess
+import json
 
 from   config.logger       import log
 
 from   config.orm          import db
-from   models.upload_model import Pending, Media, MediaStatusEnum
+from   models.upload_model import Media, MediaStatusEnum
 
 from   confluent_kafka     import Consumer, TopicPartition
 
@@ -21,6 +23,7 @@ kconsumer.subscribe([os.environ["KAFKA_TOPIC"]])
 def kafka_consumer_routine(current_app_context):
     current_app_context.push()
     thread_local_db_session = db.session()
+
     while True:
         msg = kconsumer.poll()  # get msg
 
@@ -31,8 +34,10 @@ def kafka_consumer_routine(current_app_context):
             continue
 
 
-        # decode the media_uuid from the msg
-        media_uuid = msg.value().decode("utf-8")
+        # decode the msg
+        decoded_msg = json.loads(msg.value().decode("utf-8"))
+        media_uuid  = decoded_msg["media_uuid"]
+        oauth_sub   = decoded_msg["oauth_sub"]
 
         log.info(f"kconsumer - media uuid <{media_uuid}> received for media2hls")
 
@@ -41,48 +46,53 @@ def kafka_consumer_routine(current_app_context):
         uploaded_file_path = os.path.join(f"{os.environ["UPLOAD_FOLDER"]}/", "temp/", f"{media_uuid}.dat")
         if not os.path.exists(uploaded_file_path):
             # delete dangling db record if file not found on disk
-            thread_local_db_session.execute(db.delete(Pending).where(Pending.uuid == media_uuid))
+            thread_local_db_session.execute(db.delete(Media).where(Media.uuid == media_uuid))
             thread_local_db_session.commit()
-            kconsumer.commit(offsets=[TopicPartition(msg.topic(), msg.partition(), msg.offset())])
+            kconsumer.commit(offsets=[TopicPartition(msg.topic(), msg.partition(), msg.offset() + 1)])
             log.error(f"kconsumer - media uuid <{media_uuid}> not found on disk")
             continue
 
         # mark its db record as processing
-        response = thread_local_db_session.execute(db.update(Pending).where(Pending.uuid == media_uuid).values(media_status=MediaStatusEnum.Processing.value))
+        response = thread_local_db_session.execute(db.update(Media).where(Media.uuid == media_uuid).values(media_status=MediaStatusEnum.Processing.value))
         if response.rowcount >= 1:
             thread_local_db_session.commit()
         else:
             os.remove(uploaded_file_path)  # else, delete that dangling file if not found on db
-            kconsumer.commit(offsets=[TopicPartition(msg.topic(), msg.partition(), msg.offset())])
+            kconsumer.commit(offsets=[TopicPartition(msg.topic(), msg.partition(), msg.offset() + 1)])
             log.error(f"kconsumer - media uuid <{media_uuid}> not found in db")
             continue
 
 
         # transcode it to HLS
-        result = media2hls(uploaded_file_path)
+        result = media2hls(uploaded_file_path, os.path.join(f"{os.environ["UPLOAD_FOLDER"]}/", f"{oauth_sub}/", f"{media_uuid}/"), media_uuid)
         if result:
-            #if successful, mark it on db as ready (TODO: transfer record to Media table)
-            response = thread_local_db_session.execute(db.update(Pending).where(Pending.uuid == media_uuid).values(media_status=MediaStatusEnum.Ready.value))
-            thread_local_db_session.commit()
+            #if successful, mark it on db as ready
+            response = thread_local_db_session.execute(db.update(Media).where(Media.uuid == media_uuid).values(media_status=MediaStatusEnum.Ready.value))
             log.info(f"kconsumer - media uuid <{media_uuid}> is ready for playback")
         else:
-            thread_local_db_session.execute(db.delete(Pending).where(Pending.uuid == media_uuid))  # else delete from db
-            thread_local_db_session.commit()
+            thread_local_db_session.execute(db.delete(Media).where(Media.uuid == media_uuid))  # else delete from db
             log.error(f"kconsumer - media uuid <{media_uuid}> failed to transcode to HLS")
 
 
-        os.remove(uploaded_file_path)  # and delete the file, regardless
+        thread_local_db_session.commit()
+        os.remove(uploaded_file_path)  # and delete the original upload, regardless
 
-        kconsumer.commit(offsets=[TopicPartition(msg.topic(), msg.partition(), msg.offset())])
+        kconsumer.commit(offsets=[TopicPartition(msg.topic(), msg.partition(), msg.offset() + 1)])
 
 
 
 
-def media2hls(uploaded_file_path):
-    #if not os.path.exists(temp_upload_path):
-        #os.mkdir(temp_upload_path)
-    # then move it to the /storage/sub/uuid.mp4 OR delete if failed
-    log.info(f"<<<<<{uploaded_file_path}>>>>>")
+def media2hls(uploaded_file_path, output_folder, media_uuid):
+    log.info(f"kconsumer - media uuid <{media_uuid}> is currently being encoded ...")
+
+    # set up output dir
+    os.makedirs(output_folder, exist_ok=True)
+
+    # encode to HLS!
+    cmdstr = f"ffmpeg -y -i {uploaded_file_path} -map 0:v:0? -map 0:a:0? -preset fast -codec:v libx264 -crf 22 -codec:a aac -b:a 128k -ac 2 -ar 48000 -hls_time 10 -hls_playlist_type vod -hls_flags independent_segments -f hls {output_folder}/playlist.m3u8"
+    proc   = subprocess.run(cmdstr, capture_output=True, shell=True)
+    if proc.returncode != 0:
+        return False
 
     return True
 
