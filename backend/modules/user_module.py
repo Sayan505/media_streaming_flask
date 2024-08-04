63acd654c8a125c0623bf09c952728e73b6f2be6
Sayan Bhattacharjee <bsayan8086@gmail.com>
2024-08-05 00:22:19 +0530

    feat: implement username changing

--- backend/modules/user_module.py
+++ backend/modules/user_module.py
@@ -1,23 +1,74 @@
 from flask import Blueprint
 blueprint = Blueprint("user_module", __name__)
 
+import string
 
-from config.orm        import db
-from models.user_model import User
+from   flask             import request
 
-from flask_jwt_extended import jwt_required, get_jwt_identity
+from   config.orm        import db
+from   models.user_model import User
+
+from   flask_jwt_extended import jwt_required, get_jwt_identity
+
+
+def display_name_filter(display_name):
+    allowed_charset = set(string.ascii_letters + string.digits + string.punctuation + ' ')
+
+    display_name = display_name.strip()
+    if not (len(display_name) >= 3 and len(display_name) <= 32):
+        return None, "display_name should be alteast 3 characters long and within 32 characters"
+
+    if set(display_name) <= allowed_charset:
+        return display_name, None
+    else: return None, "display_name contains disallowed characters"
+
+
 
 
 @blueprint.route("/api/me", methods=["GET"])
 @jwt_required()
-def myinfo():
-    oauth_sub = get_jwt_identity()
-    user      = db.session.execute(db.select(User).filter_by(oauth_sub=oauth_sub)).scalar_one_or_none()
+def view_self_info():
+    jwt_oauth_sub  = get_jwt_identity()
+    user           = db.session.execute(db.select(User).where(User.oauth_sub == jwt_oauth_sub)).scalar_one_or_none()
     if not user:
         return { "status": "invalid oauth identity" }, 401
 
     return {
         "display_name": user.display_name,
-        "email": user.email
+        "email":        user.email
     }, 200
+
+
+@blueprint.route("/api/me", methods=["PUT"])
+@jwt_required()
+def edit_self_info():
+    # verify ident
+    jwt_oauth_sub  = get_jwt_identity()
+    user_oauth_sub = db.session.execute(db.select(User.oauth_sub).where(User.oauth_sub == jwt_oauth_sub)).scalar_one_or_none()
+    if not user_oauth_sub:
+        return { "status": "invalid oauth identity" }, 401
+
+
+    # parse req json body
+    req_json = request.get_json(force=True, silent=True, cache=False)
+    if not req_json:
+        return { "status": "bad request" }, 400
+
+    if "display_name" not in req_json:
+        return { "status": "display_name not supplied" }, 400
+
+    new_display_name  = req_json["display_name"]
+
+    new_display_name_filtered = display_name_filter(new_display_name)
+    if not new_display_name_filtered[0]:
+        return { "status": new_display_name_filtered[1] }, 422
+
+
+    # exec query (match uuid and ownership)
+    response = db.session.execute(db.update(User).where(User.oauth_sub == user_oauth_sub).values(display_name=new_display_name_filtered))
+    if response.rowcount >= 1:
+        db.session.commit()
+        return { "status": "success" }, 200
+
+    return { "status": "bad request" }, 400
 
