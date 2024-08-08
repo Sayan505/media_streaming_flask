b73b688c6de28fb1b5547118e2fb57cd1f282057
Sayan Bhattacharjee <bsayan8086@gmail.com>
2024-08-08 13:57:13 +0530

    fix: add record creation timestamps

--- backend/models/upload_model.py
+++ backend/models/upload_model.py
@@ -1,4 +1,5 @@
 import enum
+from datetime import datetime, timezone
 
 from sqlalchemy.orm import Mapped, mapped_column
 
@@ -12,15 +13,17 @@ class MediaStatusEnum(enum.Enum):
     Ready      = "ready"
 
 class MediaTypeEnum(enum.Enum):
-    Audio      = "audio"
-    Video      = "video"
+    Audio        = "audio"
+    Video        = "video"
+    VideoNoSound = "video_nosound"
 
 
 class Media(db.Model):
-    id:                Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    uuid:              Mapped[str] = mapped_column(nullable=False, unique=True)
-    ownedby_oauth_sub: Mapped[str] = mapped_column(nullable=False)
-    media_type:        Mapped[str] = mapped_column(nullable=False)
-    title:             Mapped[str] = mapped_column(nullable=False)
-    media_status:      Mapped[str] = mapped_column(nullable=False)
+    id:                Mapped[int]      = mapped_column(primary_key=True, autoincrement=True)
+    uuid:              Mapped[str]      = mapped_column(nullable=False,   unique=True)
+    ownedby_oauth_sub: Mapped[str]      = mapped_column(nullable=False)
+    media_type:        Mapped[str]      = mapped_column(nullable=False)
+    title:             Mapped[str]      = mapped_column(nullable=False)
+    media_status:      Mapped[str]      = mapped_column(nullable=False)
+    upload_date:       Mapped[datetime] = mapped_column(default=lambda:   datetime.now(timezone.utc))
 
--- backend/models/user_model.py
+++ backend/models/user_model.py
@@ -1,4 +1,5 @@
 import enum
+from datetime import datetime, timezone
 
 from sqlalchemy.orm import Mapped, mapped_column
 
@@ -11,9 +12,10 @@ class UserRoleEnum(enum.Enum):
 
 
 class User(db.Model):
-    id:           Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    oauth_sub:    Mapped[str] = mapped_column(nullable=False, unique=True)
-    display_name: Mapped[str] = mapped_column(nullable=False)
-    email:        Mapped[str] = mapped_column(default="")
-    user_role:    Mapped[str] = mapped_column(nullable=False, default=UserRoleEnum.Audience.value)
+    id:           Mapped[int]      = mapped_column(primary_key=True, autoincrement=True)
+    oauth_sub:    Mapped[str]      = mapped_column(nullable=False,   unique=True)
+    display_name: Mapped[str]      = mapped_column(nullable=False)
+    email:        Mapped[str]      = mapped_column(default="")
+    user_role:    Mapped[str]      = mapped_column(nullable=False,   default=UserRoleEnum.Uploader.value)
+    signup_date:  Mapped[datetime] = mapped_column(default=lambda:   datetime.now(timezone.utc))
 
