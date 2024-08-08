0269a144833b00a4add32876018e45b2264efe94
Sayan Bhattacharjee <bsayan8086@gmail.com>
2024-08-08 16:27:17 +0530

    fix: add media ownership field to the mappings

--- backend/models/elasticsearch_mappings.py
+++ backend/models/elasticsearch_mappings.py
@@ -1,8 +1,9 @@
 mappings = {
     "properties": {
-        "media_title": { "type": "text"    },
-        "media_uuid":  { "type": "keyword" },
-        "media_type":  { "type": "keyword" }
+        "media_title":             { "type": "text"    },
+        "media_uuid":              { "type": "keyword" },
+        "media_type":              { "type": "keyword" },
+        "media_ownedby_oauth_sub": { "type": "keyword" }
     }
 }
 
